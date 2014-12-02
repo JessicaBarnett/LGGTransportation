1fb3190a2e682caaac7129d75994e5390e64f88c
JessicaBarnett <BarnettEffect@gmail.com>
2014-12-02 18:12:15 -0500

    the mobile navigation drawer is working.  working on individual pages now

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-$(function() {
+// $(function() {
   $('#slides').slidesjs({
     width: 940,
     height: 528
@@ -7,4 +7,85 @@ $(function() {
   $(".slidesjs-pagination-item a").empty().append($('<span class="fa fa-circle"></span>'));
   $(".slidesjs-previous").empty().append($('<span class="fa fa-chevron-left"></span>'));
   $(".slidesjs-next").empty().append($('<span class="fa fa-chevron-right"></span>'));
-});
+
+//********  Drawer for mobile nav  *********/
+
+$.fn.Drawer = function($element){
+	//drawer example at: http://www.savthecoder.com/blog/drawer-ui-jquery
+	var browserWidth = $(document).width();
+	var drawerIsOpen = false;
+
+	function refreshBrowserWidth(){
+		browserWidth = $(document).width();
+	}
+
+	function openDrawer(){
+		$element.animate({
+		        right: 0
+		}, 300);
+		drawerIsOpen = true;
+	}
+
+	function closeDrawer(){
+	    $element.show();
+	    $element.animate({
+	        right: -(browserWidth * .5) //-50%
+	    }, 300);
+	    drawerIsOpen = false;
+	}
+
+	return {
+		toggleDrawer: function () {
+			refreshBrowserWidth();
+		    if (drawerIsOpen) {
+		        closeDrawer();
+		    } else {
+		        openDrawer();
+		    }
+		}
+
+	}
+}
+
+function setNav(){
+
+	var tabletMQ = window.matchMedia("screen and (max-width: 767px)");
+	var drawer;
+
+	//creates mobile navigation.
+	if (tabletMQ.matches){
+		$("body").prepend($('<nav class="mobileNavDrawer">' +
+			'<ul>'+
+				'<li><a class="mobileNavLink" href="about.html">About Us</a></li>' +
+				'<li><a class="mobileNavLink" href="#">Services</a></li>' +
+				'<li><a class="mobileNavLink" href="#">Who Pays?</a></li> '+
+				'<li><a class="mobileNavLink" href="#">Schedule a Transport</a></li>' +
+				'<li><a class="mobileNavLink" href="#">Contact Us</a></li>' +
+			'</ul>' +
+		'</nav>'));
+
+		//creates drawer object to manipulate nav as a drawer
+		drawer = $.fn.Drawer($(".mobileNavDrawer"));
+		//removes old handlers
+		$(".hamburger").unbind();  
+		//adds click handler to open/close drawer
+		$(".hamburger").click(drawer.toggleDrawer);
+
+	}
+	else{
+		drawer = null;
+		$(".mobileNavDrawer").remove();
+		$(".hamburger").unbind();  
+	}
+
+}
+
+//runs setNav when page is loaded, and every time the page is resized
+$(window).ready(setNav);
+$(window).resize(setNav);
+
+
+
+
+
+// });
